7ad9318b1c6a87743802143433fff41048b53c78
Wesley Reis <wesleyreisolv@gmail.com>
2024-10-18 12:05:22 -0300

    First

---
+++ frontend/src/pages/Home/HomeComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Home do Dashboard</p>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/pages/Login/LoginComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Login do Site</p>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+
+</style>
